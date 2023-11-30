51eb2218d1027614f2d8aeff301599063d5f14a5
Julian Posadas <julianp44@gmail.com>
2023-11-29 20:13:11 -0800

    Add project files.

---
+++ Services/BookService.cs
@@ -0,0 +1,38 @@
+﻿using BookStoreApi.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace BookStoreApi.Services;
+
+public class BooksService
+{
+    private readonly IMongoCollection<Book> _booksCollection;
+
+    public BooksService(
+        IOptions<BookStoreDatabaseSettings> bookStoreDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(
+            bookStoreDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            bookStoreDatabaseSettings.Value.DatabaseName);
+
+        _booksCollection = mongoDatabase.GetCollection<Book>(
+            bookStoreDatabaseSettings.Value.BooksCollectionName);
+    }
+
+    public async Task<List<Book>> GetAsync() =>
+        await _booksCollection.Find(_ => true).ToListAsync(); // Returns all documents in the collection matching the provided search criteria.
+
+    public async Task<Book?> GetAsync(string id) =>
+        await _booksCollection.Find(x => x.Id == id).FirstOrDefaultAsync(); // Returns all documents in the collection matching the provided search criteria.
+
+    public async Task CreateAsync(Book newBook) =>
+        await _booksCollection.InsertOneAsync(newBook); // Inserts the provided object as a new document in the collection.
+
+    public async Task UpdateAsync(string id, Book updatedBook) =>
+        await _booksCollection.ReplaceOneAsync(x => x.Id == id, updatedBook); // Replaces the single document matching the provided search criteria with the provided object.
+
+    public async Task RemoveAsync(string id) =>
+        await _booksCollection.DeleteOneAsync(x => x.Id == id); //Deletes a single document matching the provided search criteria.
+}
