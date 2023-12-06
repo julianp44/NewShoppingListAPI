f9ef601723bf76a319d66690359e09277e011176
Julian Posadas <julianp44@gmail.com>
2023-12-05 23:21:26 -0800

    Notes from my last PUSH: - General modifications to app.html text to adhere to ShoppingListApi - modified CreateNewBook Form to adhere to ShoppingListApi - commented out code unapplicable to ShoppingListApi - created separate files and included them into app.html for new Javascript and CSS: ShoppingListForm.css, Display_NL_FormToggle.css

---
+++ wwwroot/js/Display_NL_FormToggle.js
@@ -0,0 +1,10 @@
+﻿const showFormBtn = document.getElementById('show-form-btn');
+const myForm = document.getElementById('ShoppingListForm');
+
+showFormBtn.addEventListener('click', () => {
+    if (myForm.style.display === 'none') {
+        myForm.style.display = 'block';
+    } else {
+        myForm.style.display = 'none';
+    }
+});
